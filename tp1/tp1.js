7f021e68219aff771171723b779479636db1cbe6
Valentina Chavez <valenchavez1908@gmail.com>
2026-09-01 16:40:29 +0200

    Add files via upload

--- tp1/tp1.js
+++ tp1/tp1.js
@@ -10,17 +10,17 @@ let x = 25;
 
 //variables para la escena 0
 let areaX1 = 300;
-let areaY1 = 260;
+let areaY1 = 180;
 let pos1 = false;
 
 let areaX2 = 200;
-let areaY2 = 260;
+let areaY2 = 180;
 let pos2 = false;
 
 let estado = 0;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 600);
 
   //carga de imagenes
   for (let i=0; i<3; i++) {
@@ -50,13 +50,13 @@ function draw() {
 
   if (estado===0) {
 
-    image(fondo[spriteFon], 0, 0, width, 700);
+    image(fondo[spriteFon], 0, -80, width, 700);
 
-    image(caminata[5], 450, 30, 500, 700);
+    image(caminata[5], 450, -50, 500, 700);
 
     //rectangulos que marcan a donde arrastrar
-    rect(510, 300, 30, 60);
-    rect(565, 300, 30, 60);
+    rect(510, 220, 30, 60);
+    rect(565, 220, 30, 60);
 
     if (pos1) {
       areaX1 = mouseX - 25;
@@ -73,30 +73,53 @@ function draw() {
     rect(areaX2, areaY2, 30, 60);
 
     //la distancia ayuda a que no sea en los pixeles exactos
-    let dist1 = dist(areaX1, areaY1, 510, 300);
-    let dist2 = dist(areaX2, areaY2, 565, 300);
+    let dist1 = dist(areaX1, areaY1, 510, 220);
+    let dist2 = dist(areaX2, areaY2, 565, 220);
 
     if (dist1 < 15 && dist2 < 15 && !pos1 && !pos2) { // si cumple el lugar y si se solto el bloque
       estado=1;
       //1
       areaX1 = 510;
-      areaY1 = 300;
+      areaY1 = 220;
       //2
       areaX2 = 565;
-      areaY2 = 300;
+      areaY2 = 220;
       //para caminata
       x = 300;
     } else {
     }
     
   } else if (estado===1) {
-    image(fondo[spriteFon], 0, 0, width, 700);
-    x += 5;
+    image(fondo[spriteFon], 0, -80, width, 700);
+    x += 10;
     caminar(); //llamar funcion para que se haga el cambio
-    image(caminata[spriteCam], x, 30, 500, 700);
+    image(caminata[spriteCam], x, -50, 500, 700);
     cambio();
     
   } else if (estado===2) {
+    image(fondo[spriteFon], 0, -80, width, 700);
+    
+    image(mesas[0], 100, -10, 400, 600);
+    image(mesas[3], 120, -10, 400, 600);
+    image(clientes[1], 120, 0, 400, 600);
+      
+    x += 10;
+    caminar(); //llamar funcion para que se haga el cambio
+    image(caminata[spriteCam], x, -50, 500, 700);
+    cambio();
+    
+  } else if (estado==3) {
+    image(fondo[spriteFon], 0, -80, width, 700);
+    
+    image(mesas[3], 120, -10, 400, 600);
+    image(mesas[1], 100, -10, 400, 600);
+    image(clientes[0], 120, -10, 400, 600);
+    image(mesas[2], 100, -10, 400, 600);
+    
+    x += 10;
+    caminar(); //llamar funcion para que se haga el cambio
+    image(caminata[spriteCam], x, -50, 500, 700);
+    cambio();
   }
 }
 
@@ -108,6 +131,9 @@ function caminar() {
 }
 
 function mousePressed() {
+  
+  console.log(mouseX, mouseY);
+  
   if (pos1) {
     pos1 = false; //soltar
   } else {
@@ -130,7 +156,7 @@ function mousePressed() {
 function cambio() {
   //cambio de escena
   if (x>width+500) {
-    x = -500;
+    x = -300;
     estado++;
     spriteFon++;
 
@@ -144,4 +170,5 @@ function cambio() {
     console.log("escena ", estado, " cargada");
     console.log("fondo ", spriteFon, " cargada");
   }
+  
 }
